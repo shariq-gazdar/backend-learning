42b6323cd08d1434b2a269e67581f4ad09dbc0e0
= <gazdarshariq0@gmail.com>
2025-03-05 12:44:20 +0500

    machine 02 added!

--- express-js/server.js
+++ express-js/server.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
+const parse = require("parse");
+const bodyParser = require("body-parser");
 const port = 5000;
+// app.use(parse());
+app.use(bodyParser());
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  const jsonData = {
+    name: "Shariq Gazdar",
+    role: "Author",
+  };
+  res.send(jsonData);
+});
+app.get("/user", (req, res) => {
+  const { email, password } = req.body;
+  //   const userId = email + password;
+  //   res.send(userId);
+  console.log(email);
+  res.send("user route");
+
+  //   res.send(req.body);
 });
 app.listen(port, () => {
-  console.log("server running on port", port);
+  console.log("server running on port:", port);
 });
